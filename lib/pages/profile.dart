a2bcb966b2785911cadb6562a9978cdf39020349
tahaali73 <diytech960@gmail.com>
2024-06-10 02:17:25 -0700

    completed

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -19,18 +19,20 @@ class cloneprofile extends StatelessWidget {
                   child: Container(
                 height: h/2,
                 width: w,
-                color: Colors.amberAccent,
+                //color: Colors.amberAccent
               )),
               Positioned(
                   child: Container(
                 height: h/2.3,
                 width: w,
-                color: Colors.red,
+                //color: Colors.red,
+                    decoration: BoxDecoration(image: DecorationImage(image: NetworkImage("https://tse3.mm.bing.net/th?id=OIP.ZwgbwolAaVhlsI57X9ptdQHaEh&pid=Api&P=0&h=220"),fit: BoxFit.cover)),
               )),
               Positioned(
                   bottom: 0,
                   right: 20,
-                  child: CircleAvatar(radius: 50,backgroundColor: Colors.black,))
+                  child: CircleAvatar(backgroundImage: NetworkImage("https://tse3.mm.bing.net/th?id=OIP.X7RDB_jcDDBdCiwVQiPctQHaIl&pid=Api&P=0&h=220"),
+                    radius: 50,))
             ],),
               Container(
                // color: Colors.blue,
@@ -44,7 +46,7 @@ class cloneprofile extends StatelessWidget {
                      Text('this city atract lots of tourist every year',style: TextStyle(fontSize: 12,fontWeight: FontWeight.w300),),
                     Padding(
                       padding: const EdgeInsets.all(5.0),
-                      child: Divider(height: 5,color: Colors.black,),
+                      child: Divider(height: 5,color: Colors.white,),
                     ),
                      Row(
                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -53,8 +55,19 @@ class cloneprofile extends StatelessWidget {
                        iconwittext('255', Icons.share_rounded),
                        iconwittext('578', Icons.comment_rounded),
                        iconwittext('24563', Icons.add_reaction_rounded),
-                     ],)
-                  ],),)
+                     ],),
+                  ],),),
+              Divider(height: 5,thickness: 1,color: Colors.black,),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Text('Barcelona. I don’t think there is much point in praising a city where it is so easy to love,'
+                    ' so visually appealing and emotionally pleasing'
+                    ' (if I were describing a person that way, I would probably'
+                    ' get married). You will love Barcelona, you will definitely'
+                    ' want to go back at some point, you will be filled with the desire'
+                    ' to take pictures and document this beauty every second while'
+                    ' walking.',style: TextStyle(fontSize: 10),),
+              )
                    ],),),
          ),
       
